c4b48b17858313dfca4620cd52d5fdb73a96a467
Visal .In <invisal@gmail.com>
2023-08-30 18:49:54 +0700

    add banner

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'invisal/profile - One Stop Site About Visal .In';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "invisal/profile - One Stop Site About Visal .In";
+export const SITE_URL = "https://invisal.com";
+export const SITE_DESCRIPTION =
+  "Welcome to my personal website. Learn everything about me.";
